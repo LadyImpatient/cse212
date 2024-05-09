981c77ca88684e135ffdedc3ed96b817cf2b6c9c
LadyImpatient <aliciapuuwai@gmail.com>
2024-05-09 18:21:27 -0500

    takingturnsqueue modified for test 1

--- week02/code/TakingTurns.cs
+++ week02/code/TakingTurns.cs
@@ -6,16 +6,17 @@
         // Test 1
         // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3) and
         // run until the queue is empty
-        // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
+        // Expected Result: 
+        //Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
         Console.WriteLine("Test 1");
         var players = new TakingTurnsQueue();
         players.AddPerson("Bob", 2);
         players.AddPerson("Tim", 5);
         players.AddPerson("Sue", 3);
-        // Console.WriteLine(players);    // This can be un-commented out for debug help
+        Console.WriteLine(players);    // This can be un-commented out for debug help
         while (players.Length > 0)
             players.GetNextPerson();
-        // Defect(s) Found: None
+        // Defect(s) Found: There's no rotation between Persons to take turns
 
         Console.WriteLine("---------");
 
@@ -23,66 +24,65 @@
         // Scenario: Create a queue with the following people and turns: Bob (2), Tim (5), Sue (3)
         // After running 5 times, add George with 3 turns.  Run until the queue is empty.
         // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, George, Sue, Tim, George, Tim, George
-        Console.WriteLine("Test 2");
-        players = new TakingTurnsQueue();
-        players.AddPerson("Bob", 2);
-        players.AddPerson("Tim", 5);
-        players.AddPerson("Sue", 3);
-        for (int i = 0; i < 5; i++) {
-            players.GetNextPerson();
-            // Console.WriteLine(players);
-        }
+        //Console.WriteLine("Test 2");
+        //players = new TakingTurnsQueue();
+        //players.AddPerson("Bob", 2);
+        //players.AddPerson("Tim", 5);
+        //players.AddPerson("Sue", 3);
+        //Console.WriteLine(players);
+        //for (int i = 0; i < 5; i++) {
+        //    Console.WriteLine(players.GetNextPerson());
+        //    players.AddPerson("George", 3);
+        //}
+         //Console.WriteLine(players);
+        //while (players.Length > 0)
+         //   players.GetNextPerson();
 
-        players.AddPerson("George", 3);
-        // Console.WriteLine(players);
-        while (players.Length > 0)
-            players.GetNextPerson();
+        // Defect(s) Found: Displaying the list instead of the containers of the list.
 
-        // Defect(s) Found: None
-
-        Console.WriteLine("---------");
+        //Console.WriteLine("---------");
 
         // Test 3
         // Scenario: Create a queue with the following people and turns: Bob (2), Tim (Forever), Sue (3)
         // Run 10 times.
         // Expected Result: Bob, Tim, Sue, Bob, Tim, Sue, Tim, Sue, Tim, Tim
-        Console.WriteLine("Test 3");
-        players = new TakingTurnsQueue();
-        players.AddPerson("Bob", 2);
-        players.AddPerson("Tim", 0);
-        players.AddPerson("Sue", 3);
+        //Console.WriteLine("Test 3");
+        //players = new TakingTurnsQueue();
+        //players.AddPerson("Bob", 2);
+        //players.AddPerson("Tim", 0);
+        //players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
-            players.GetNextPerson();
+        //for (int i = 0; i < 10; i++) {
+        //    players.GetNextPerson();
             // Console.WriteLine(players);
-        }
+        //}
         // Defect(s) Found: None
 
-        Console.WriteLine("---------");
+        //Console.WriteLine("---------");
 
          // Test 4
         // Scenario: Create a queue with the following people and turns: Tim (Forever), Sue (3)
         // Run 10 times.
         // Expected Result: Tim, Sue, Tim, Sue, Tim, Sue, Tim, Tim, Tim, Tim
-        Console.WriteLine("Test 4");
-        players = new TakingTurnsQueue();
-        players.AddPerson("Tim", -3);
-        players.AddPerson("Sue", 3);
+        //Console.WriteLine("Test 4");
+        //players = new TakingTurnsQueue();
+        //players.AddPerson("Tim", -3);
+        //players.AddPerson("Sue", 3);
         // Console.WriteLine(players);
-        for (int i = 0; i < 10; i++) {
-            players.GetNextPerson();
+        //for (int i = 0; i < 10; i++) {
+        //    players.GetNextPerson();
             // Console.WriteLine(players);
-        }
+        //}
         // Defect(s) Found: None
 
-        Console.WriteLine("---------");
+        //Console.WriteLine("---------");
 
         // Test 5
         // Scenario: Try to get the next person from an empty queue
         // Expected Result: Error message should be displayed
-        Console.WriteLine("Test 5");
-        players = new TakingTurnsQueue();
-        players.GetNextPerson();
+        //Console.WriteLine("Test 5");
+        //players = new TakingTurnsQueue();
+        //players.GetNextPerson();
         // Defect(s) Found: None
     }
 }
--- week02/code/TakingTurnsQueue.cs
+++ week02/code/TakingTurnsQueue.cs
@@ -8,9 +8,9 @@
 /// not be added back into the queue.
 /// </summary>
 public class TakingTurnsQueue {
-    private readonly PersonQueue _people = new();
+    private readonly Queue<Person> _people = new Queue<Person>();
 
-    public int Length => _people.Length;
+    public int Length => _people.Count;
 
     /// <summary>
     /// Add new people to the queue with a name and number of turns
@@ -30,20 +30,25 @@ public class TakingTurnsQueue {
     /// if the queue is empty.
     /// </summary>
     public void GetNextPerson() {
-        if (_people.IsEmpty())
+        if (_people.Count==0) {
             Console.WriteLine("No one in the queue.");
-        else {
-            Person person = _people.Dequeue();
-            if (person.Turns > 1) {
-                person.Turns -= 1;
-                _people.Enqueue(person);
+            return;
             }
-
+            Person person= _people.Dequeue();
             Console.WriteLine(person.Name);
+        if (person.Turns>1) || person.Turns < 0 {
+            if (person.Turns > 0)
+                person.Turns -= 1;
+            _people.Enqueue(person);
+            }
         }
     }
 
     public override string ToString() {
-        return _people.ToString();
+        StringBuilder sb = new StringBuilder();
+        foreach (var person in _people) {
+            sb.Append($"({person.Name}:{person.Turns})");
+        }
+        return sb.ToString().TrimEnd(' ', ' , ');
     }
 }
